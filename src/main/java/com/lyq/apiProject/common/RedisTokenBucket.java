13a454169961b316b7fa19b63ce0d1ee6e22a2e3
liyuqi <1175881864@qq.com>
2025-01-09 22:38:29 +0800

    完成令牌桶算法

--- src/main/java/com/lyq/apiProject/common/RedisTokenBucket.java
+++ src/main/java/com/lyq/apiProject/common/RedisTokenBucket.java
@@ -1,9 +1,11 @@
 package com.lyq.apiProject.common;
 
+import com.lyq.apiProject.constant.RedisConstant;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class RedisTokenBucket {
@@ -21,6 +23,33 @@ public class RedisTokenBucket {
      * @return
      */
     public boolean tryAcquire(String phoneNumber) {
-        return true;
+        int     permitsPerMinute = 1; // 每个手机号码一分钟内只能发送一条短信
+        int     maxPermits       = 1; // 令牌桶容量
+        int     tokensNum        = 0; // 已有的令牌数
+        long    currentTime      = System.currentTimeMillis(); // 当前时间戳
+        long    lastRefillTime   = 0; //令牌桶上次填充的时间戳
+
+        String key = RedisConstant.SMS_BUCKET_PREFIX + phoneNumber;
+        String tokens = redisTemplate.opsForValue().get(key + "_tokens");
+        if (tokens != null) {
+            tokensNum = Integer.parseInt(tokens);
+        }
+        String lastRefillTimeStr = redisTemplate.opsForValue().get(key + "_last_refill_time");
+        if (lastRefillTimeStr != null) {
+            lastRefillTime = Long.parseLong(lastRefillTimeStr);
+        }
+
+        // 通过当前与上一次填充的时间差，计算出需要填充的令牌数
+        int refillTokens = (int) (currentTime-lastRefillTime) / 1000 / 60 * permitsPerMinute;
+        tokensNum = Math.min(maxPermits, refillTokens + tokensNum);
+        // 更新上次填充时间戳
+        redisTemplate.opsForValue().set(key + "_last_refill_time", String.valueOf(currentTime),EXPIRE_TIME, TimeUnit.SECONDS);
+        // 如果令牌数大于等于1，则获取令牌
+        if (tokensNum >= 1) {
+            tokensNum--;
+            redisTemplate.opsForValue().set(key + "_tokens", String.valueOf(tokensNum),EXPIRE_TIME, TimeUnit.SECONDS);
+            return true; // 获取到令牌，返回true
+        }
+        return false; // 没有获取到令牌，返回false
     }
 }
